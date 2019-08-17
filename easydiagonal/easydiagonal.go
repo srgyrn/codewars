ba53ed7bd7027ebe2b77e549e8803cd24e50764d
Sera Geyran <sgeyran@gmail.com>
2019-08-17 17:55:41 +0300

    easy diagonal v.3

--- easydiagonal/easydiagonal.go
+++ easydiagonal/easydiagonal.go
@@ -3,12 +3,6 @@ package easydiagonal
 import "math/big"
 
 func Diagonal(n, p int) int {
-	var result, binomial1, binomial2 big.Int
-
-	binomial1.Binomial(int64(n), int64(p))
-	binomial2.Binomial(int64(n), int64(p+1))
-
-	result = *result.Add(&binomial1, &binomial2)
-
-	return int(result.Int64())
+	var binomial big.Int
+	return int(binomial.Binomial(int64(n+1), int64(p+1)).Int64())
 }
